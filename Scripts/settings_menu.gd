5fbf622a99d30dbfcc96de604075fb30c1cf087b
AbrahamReyes08 <124213092+AbrahamReyes08@users.noreply.github.com>
2023-11-01 14:35:36 -0600

    settings2

--- Scripts/settings_menu.gd
+++ Scripts/settings_menu.gd
@@ -17,4 +17,4 @@ func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 
 func _on_brillo_slide_2_value_changed(value):
-	GlobalWorldEnvironment.environment.adjustment_brightness=value
+	GlobalWorldEnvironment.environment.adjustment_brightness = value
