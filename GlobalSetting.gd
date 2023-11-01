77b840f1cab1a329031c22b17b294476982e697f
AbrahamReyes08 <124213092+AbrahamReyes08@users.noreply.github.com>
2023-11-01 14:25:53 -0600

    settings

---
+++ GlobalSetting.gd
@@ -0,0 +1,8 @@
+extends Node
+
+signal brightness_updated(value)
+
+func update_brightness(value):
+	emit_signal("brightness_updated",value)
+
+
--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -7,3 +7,7 @@ func _on_jugar_pressed():
 
 func _on_salir_pressed():
 	get_tree().quit()
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/settings_menu.tscn")
