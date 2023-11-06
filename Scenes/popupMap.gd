fbef5a308062a6ff19f286c0ba72af5d2ba0c3f5
AbrahamReyes08 <124213092+AbrahamReyes08@users.noreply.github.com>
2023-11-05 18:44:49 -0600

    popup

--- Scenes/popupMap.gd
+++ Scenes/popupMap.gd
@@ -9,3 +9,13 @@ func _on_button_2_mouse_entered():
 
 func _on_button_3_mouse_entered():
 	get_tree().get_nodes_in_group("Sonidos")[0].get_node("Click").play()
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/desertbackground.tscn")
+
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://Scenes/nieveMap.tscn")
