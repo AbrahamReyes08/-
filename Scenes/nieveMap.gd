444cde0acc5dc1d81088b4e240670802e8e1b2fa
AbrahamReyes08 <124213092+AbrahamReyes08@users.noreply.github.com>
2023-11-16 14:11:12 -0600

    mapas

--- Scenes/nieveMap.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready():
-	var enemigo = $Enemigo
-	var personaje = $Personaje
-	enemigo.set_target(personaje)
-	
-func _process(delta):
-	pass
--- Scripts/popupMap.gd
+++ Scripts/popupMap.gd
@@ -12,10 +12,5 @@ func _on_button_3_mouse_entered():
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Adds/Scenes/mapa.tscn")
+	get_tree().change_scene_to_file("res://Scenes/MainWorld.tscn")
 
-func _on_button_2_pressed():
-	get_tree().change_scene_to_file("res://Scenes/desertbackground.tscn")
-
-func _on_button_3_pressed():
-	get_tree().change_scene_to_file("res://Scenes/nieveMap.tscn")
