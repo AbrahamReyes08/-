1451f5b37f810e304b4c24234ee92f0db1508b83
MMwile <danielfloresb@unitec.edu>
2023-11-18 16:54:22 -0600

    Cambios en la generación de los enemigos
    Ya no pueden spawnear enemigos en donde hay collision shapes

--- Scripts/MainWorld.gd
+++ Scripts/MainWorld.gd
@@ -17,7 +17,7 @@ func _on_enemy_spawn_timeout():
 		var position_player = personaje.position
 		var new_x = randi_range(position_player.x-210, position_player.x+210)
 		var new_y = randi_range(position_player.y-130, position_player.y+130)
-		while(((new_x>position_player.x-190 && new_x<position_player.x+190) && (new_y>position_player.y-110 && new_y<position_player.y+110)) || ((new_x<25 || new_x>1135) || (new_y<25 || new_y>625))):
+		while(((new_x>position_player.x-190 && new_x<position_player.x+190) && (new_y>position_player.y-110 && new_y<position_player.y+110)) || ((new_x<25 || new_x>1135) || (new_y<25 || new_y>625)) || (is_position_colliding(new_x, new_y))):
 			new_x = randi_range(position_player.x-210, position_player.x+210)
 			new_y = randi_range(position_player.y-130, position_player.y+130)
 		var enemy_instance = enemy.instantiate()
@@ -26,3 +26,11 @@ func _on_enemy_spawn_timeout():
 		add_child(enemy_instance)
 		cantenemy+=1
 	
+func is_position_colliding(x, y):
+	var static_body = get_node("StaticBody2D")
+	var collision_shapes = static_body.get_children()
+	for shape in collision_shapes:
+		if shape is CollisionShape2D:
+			if shape.collide_point(Vector2(x, y)):
+				return true
+	return false
