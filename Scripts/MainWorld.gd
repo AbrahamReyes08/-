678f64e1ad94f7a0c53d106bc9e4ada453697ea9
MMwile <danielfloresb@unitec.edu>
2023-11-18 16:38:20 -0600

    Enemy Generation
    Se agregó la generación de los enemigos en el mapa

--- Scripts/MainWorld.gd
+++ Scripts/MainWorld.gd
@@ -1,10 +1,28 @@
 extends Node2D
 
+@onready var enemy = preload("res://Adds/Scenes/enemigo.tscn")
+@onready var personaje = $Personaje
+var maxenemy=25
+var cantenemy=0
+
 func _ready():
-	var enemigo = $Enemigo
-	var personaje = $Personaje
+	$EnemySpawn.start()
 	$fuente.play("default")
-	enemigo.set_target(personaje)
 	
 func _process(delta):
 	pass
+
+func _on_enemy_spawn_timeout():
+	if(cantenemy<maxenemy):
+		var position_player = personaje.position
+		var new_x = randi_range(position_player.x-210, position_player.x+210)
+		var new_y = randi_range(position_player.y-130, position_player.y+130)
+		while(((new_x>position_player.x-190 && new_x<position_player.x+190) && (new_y>position_player.y-110 && new_y<position_player.y+110)) || ((new_x<25 || new_x>1135) || (new_y<25 || new_y>625))):
+			new_x = randi_range(position_player.x-210, position_player.x+210)
+			new_y = randi_range(position_player.y-130, position_player.y+130)
+		var enemy_instance = enemy.instantiate()
+		enemy_instance.position = Vector2(new_x, new_y)
+		enemy_instance.set_target(personaje)
+		add_child(enemy_instance)
+		cantenemy+=1
+	
